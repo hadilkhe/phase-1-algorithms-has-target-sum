0d80ae1a0035d102a05533f9c0916ef6788c11c4
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-02 23:06:10 +0100

    finished

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const difference = target - array[i];
+    
+    if (seenNumbers.has(difference)) {
+      return true;
+    }
+    
+    seenNumbers.add(array[i]);
+  }
+  
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
